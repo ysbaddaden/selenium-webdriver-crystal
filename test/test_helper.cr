e1e9b98d95683e1b16c16c76045f79bcee438a68
Julien Portalier <julien@portalier.com>
2016-03-23 12:52:31 +0100

    Fix: use class variables instead of global in tests
    refs https://github.com/crystal-lang/crystal/issues/2357

--- test/test_helper.cr
+++ test/test_helper.cr
@@ -2,25 +2,31 @@ require "uri"
 require "minitest/autorun"
 require "../src/selenium"
 
-begin
-  uri = URI.parse(ENV.fetch("SELENIUM_URL", "http://localhost:4444"))
-  $driver = Selenium::Webdriver.new(uri.host.not_nil!, uri.port || 4444)
-
-  $session = $driver.create_session({
-    browserName: ENV.fetch("BROWSER_NAME", "firefox"),
-    platform: "ANY",
-  })
-  $session.url = "about:blank"
-end
-
 class Minitest::Test
   def driver
-    $driver
+    self.class.driver
+  end
+
+  def self.driver
+    @@driver ||= begin
+      uri = URI.parse(ENV.fetch("SELENIUM_URL", "http://localhost:4444"))
+      Selenium::Webdriver.new(uri.host.not_nil!, uri.port || 4444)
+    end
   end
 
   def session
-    $session
+    self.class.session
+  end
+
+  def self.session
+    @@session ||= begin
+      session = driver.create_session({
+        browserName: ENV.fetch("BROWSER_NAME", "firefox"),
+        platform: "ANY",
+      })
+      session.url = "about:blank"
+      Minitest.after_run { session.stop }
+      session
+    end
   end
 end
-
-Minitest.after_run { $session.stop }
